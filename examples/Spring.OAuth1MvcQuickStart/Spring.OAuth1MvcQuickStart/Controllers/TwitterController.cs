d6a5efd4ebedc52815adbda47e4f394e50bf644c
Bruno Baia <brbaia@gmail.com>
2011-12-19 16:29:52 +0100

    Added OAuth1 ASP.NET MVC 3 QuickStart

---
+++ examples/Spring.OAuth1MvcQuickStart/Spring.OAuth1MvcQuickStart/Controllers/TwitterController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Spring.Social.OAuth1;
+using Spring.Social.Twitter.Api;
+
+namespace Spring.OAuth1MvcQuickStart.Controllers
+{
+    public class TwitterController : Controller
+    {
+        // Register your own Twitter app at https://dev.twitter.com/apps/new with "Read & Write" access type
+        // Configure the Callback URL with 'http://localhost/Twitter/Callback'
+        // Set your consumer key & secret here
+        private const string TwitterConsumerKey = TODO;
+        private const string TwitterConsumerSecret = TODO;
+
+        IOAuth1ServiceProvider<ITwitter> twitterProvider = 
+            new TwitterServiceProvider(TwitterConsumerKey, TwitterConsumerSecret);
+
+        // GET: /Twitter/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Twitter/SignIn
+        public ActionResult SignIn()
+        {
+            OAuthToken requestToken = twitterProvider.OAuthOperations.FetchRequestTokenAsync("http://localhost/Twitter/Callback", null).Result;
+            Session["RequestToken"] = requestToken;
+
+            return Redirect(twitterProvider.OAuthOperations.BuildAuthenticateUrl(requestToken.Value, OAuth1Parameters.NONE));
+        }
+
+        // GET: /Twitter/Callback
+        public ActionResult Callback(string oauth_verifier)
+        {
+            OAuthToken requestToken = Session["RequestToken"] as OAuthToken;
+            AuthorizedRequestToken authorizedRequestToken = new AuthorizedRequestToken(requestToken, oauth_verifier);
+            OAuthToken token = twitterProvider.OAuthOperations.ExchangeForAccessTokenAsync(authorizedRequestToken, null).Result;
+
+            Session["TwitterClient"] = twitterProvider.GetApi(token.Value, token.Secret);
+
+            return View();
+        }
+
+        // GET: /Twitter/UpdateStatus
+        public ActionResult UpdateStatus()
+        {
+            return View();
+        }
+
+        // POST: /Twitter/UpdateStatus
+        [HttpPost]
+        public ActionResult UpdateStatus(string status)
+        {
+            ITwitter twitterClient = Session["TwitterClient"] as ITwitter;
+
+            twitterClient.UpdateStatusAsync(status).Wait();
+
+            return View();
+        }
+    }
+}
