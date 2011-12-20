194e9f39de220fad52ccf75545ff72064c65f34e
Bruno Baia <brbaia@gmail.com>
2011-12-20 17:45:13 +0100

    Added OAuth1 Windows Phone QuickStart

---
+++ examples/Spring.OAuth1WindowsPhoneQuickStart/Spring.OAuth1WindowsPhoneQuickStart/ViewModel/TwitterViewModel.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO.IsolatedStorage;
+
+using Spring.Social.OAuth1;
+using Spring.Social.Twitter.Api;
+
+namespace Spring.OAuth1WindowsPhoneQuickStart.ViewModel
+{
+    public class TwitterViewModel : INotifyPropertyChanged
+    {
+        private const string OAuthTokenKey = "OAuthToken";
+        public const string CallbackUrl = "http://localhost/Twitter/Callback";
+
+        private OAuthToken requestOAuthToken;
+        private Uri authenticateUri;
+
+        public IOAuth1ServiceProvider<ITwitter> TwitterServiceProvider { get; set; }
+
+        public bool IsAuthenticated
+        {
+            get
+            {
+                return this.OAuthToken != null;
+            }
+        }
+
+        public OAuthToken OAuthToken
+        {
+            get
+            {
+                return this.LoadSetting<OAuthToken>(OAuthTokenKey, null);
+            }
+            set
+            {
+                this.SaveSetting(OAuthTokenKey, value);
+                NotifyPropertyChanged("IsAuthenticated");
+            }
+        }
+
+        public Uri AuthenticateUri
+        {
+            get
+            {
+                return this.authenticateUri;
+            }
+            set
+            {
+                this.authenticateUri = value;
+                NotifyPropertyChanged("AuthenticateUri");
+            }
+        }
+
+        public void Authenticate()
+        {
+            this.TwitterServiceProvider.OAuthOperations.FetchRequestTokenAsync(CallbackUrl, null,
+                r =>
+                {
+                    this.requestOAuthToken = r.Response;
+                    this.AuthenticateUri = new Uri(this.TwitterServiceProvider.OAuthOperations.BuildAuthenticateUrl(r.Response.Value, OAuth1Parameters.NONE));
+                });
+        }
+
+        public void AuthenticateCallback(string verifier)
+        {
+            AuthorizedRequestToken authorizedRequestToken = new AuthorizedRequestToken(this.requestOAuthToken, verifier);
+            this.TwitterServiceProvider.OAuthOperations.ExchangeForAccessTokenAsync(authorizedRequestToken, null,
+                r =>
+                {
+                    this.OAuthToken = r.Response;
+                });
+        }
+
+        public void UpdateStatus(string status)
+        {
+            ITwitter twitterClient = this.TwitterServiceProvider.GetApi(this.OAuthToken.Value, this.OAuthToken.Secret);
+            twitterClient.UpdateStatusAsync(status, null);
+        }
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+        private void SaveSetting(string key, object value)
+        {
+            var settings = IsolatedStorageSettings.ApplicationSettings;
+            if (settings.Contains(key))
+            {
+                settings[key] = value;
+            }
+            else
+            {
+                settings.Add(key, value);
+            }
+        }
+
+        private T LoadSetting<T>(string key, T defaultValue)
+        {
+            var settings = IsolatedStorageSettings.ApplicationSettings;
+            if (!settings.Contains(key))
+            {
+                settings.Add(key, defaultValue);
+            }
+            return (T)settings[key];
+        }
+    }
+}
---
+++ examples/Spring.OAuth1WindowsPhoneQuickStart/Spring.OAuth1WindowsPhoneQuickStart/Views/Twitter.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+
+using Spring.OAuth1WindowsPhoneQuickStart.ViewModel;
+
+namespace Spring.OAuth1WindowsPhoneQuickStart.Views
+{
+    public partial class Twitter : PhoneApplicationPage
+    {
+        public Twitter()
+        {
+            InitializeComponent();
+
+            this.DataContext = this.ViewModel;
+            this.ViewModel.PropertyChanged += new PropertyChangedEventHandler(ViewModel_PropertyChanged);
+        }
+
+        public TwitterViewModel ViewModel
+        {
+            get { return App.Current.TwitterViewModel; }
+        }
+
+        // Overrided methods
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (!this.ViewModel.IsAuthenticated)
+            {
+                this.ViewModel.Authenticate();
+            }
+        }
+
+        // Event methods
+
+        void ViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            // WebBrowser.Source property not bindable
+            if (e.PropertyName == "AuthenticateUri")
+            {
+                this.WebBrowser.Navigate(this.ViewModel.AuthenticateUri);
+            }
+        }
+
+        private void WebBrowser_Navigating(object sender, NavigatingEventArgs e)
+        {
+            if (this.ViewModel != null)
+            {
+                string url = e.Uri.ToString();
+                if (url.StartsWith(TwitterViewModel.CallbackUrl, StringComparison.OrdinalIgnoreCase))
+                {
+                    string verifier = url.Substring(url.LastIndexOf("oauth_verifier=") + 15);
+                    this.ViewModel.AuthenticateCallback(verifier);
+                }
+            }
+        }
+
+        private void TweetButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.ViewModel != null)
+            {
+                if (this.ViewModel.IsAuthenticated)
+                {
+                    this.ViewModel.UpdateStatus(this.StatusTextBox.Text);
+                }
+                else
+                {
+                    this.ViewModel.Authenticate();
+                }
+            }
+        }
+    }
+}
