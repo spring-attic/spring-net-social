9aa2453f9deb4c42f00f9db52ffa6fd6a9ae988d
Bruno Baia <brbaia@gmail.com>
2012-01-02 21:38:25 +0100

    Happy New Year 2012!

--- examples/Spring.OAuth2WindowsPhoneQuickStart/Spring.OAuth2WindowsPhoneQuickStart/Properties/AssemblyInfo.cs
+++ examples/Spring.OAuth2WindowsPhoneQuickStart/Spring.OAuth2WindowsPhoneQuickStart/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Spring.OAuth2WindowsPhoneQuickStart")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
