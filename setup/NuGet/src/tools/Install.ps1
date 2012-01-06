e061f119c55194298e6c7304e05f6669104207c5
Bruno Baia <brbaia@gmail.com>
2012-01-06 21:01:28 +0100

    Fixed release build

--- setup/NuGet/src/tools/Install.ps1
+++
@@ -1,6 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-if($project.Properties.Item("TargetFrameworkMoniker").Value.StartsWith(".NETFramework"))
-{
-	Install-Package Common.Logging -Version 2.0.0	
-}
